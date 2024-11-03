699e8bc41d3b49851189ee7508f5b6fc13be4b48
zxc777zxc <153791686+zxc777zxc@users.noreply.github.com>
2024-11-03 16:02:56 +0500

    Add files via upload
    Notification

---
+++ NotificationService.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.example.interfaces.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationService {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+}
